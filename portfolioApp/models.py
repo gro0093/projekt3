319d8580271f0befd44609176da7f01f6e307936
Patrik Grobar <patrik.grobar.st@vsb.cz>
2019-11-27 13:36:15 +0100

    prvni komit

---
+++ portfolioApp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Projekty(models.Model):
+ image = models.ImageField(upload_to = 'images/')
+ popis = models.CharField(max_length = 200)
