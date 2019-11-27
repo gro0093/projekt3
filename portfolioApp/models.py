31b3d4abaa28a5697d301277b0298b038d85a041
Patrik Grobar <patrik.grobar.st@vsb.cz>
2019-11-27 13:38:55 +0100

    uprava delky pole

--- portfolioApp/models.py
+++ portfolioApp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Projekty(models.Model):
  image = models.ImageField(upload_to = 'images/')
- popis = models.CharField(max_length = 200)
+ popis = models.CharField(max_length = 300)
